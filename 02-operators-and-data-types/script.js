2d5bbf6f342ce2b7097d755685f82499841b3d10
rayne <rayne.jurchen@gmail.com>
2024-05-20 18:22:15 +0200

    solves given problems with operators and data types

--- 02-operators-and-data-types/script.js
+++ 02-operators-and-data-types/script.js
@@ -1,1 +1,29 @@
+//Task 1): Use the concatenation operator to concatenate each letter of your firstname into a string one letter by one.
+
 console.log("R" + "a" + "y" + "n" + "e");
+
+//Task 2) spot the problems and solve them
+
+// Error! Make it work: console.log("Hallo";
+console.log("Hallo");
+
+// Error! Make it work: console.log("That doesn"t work");
+console.log("That doesn't work");
+
+// should return true. Change only the operator to fix it: console.log(50 + 2 > 52);
+console.log(50 + 2 >= 52);
+
+// should be false. Change only the operator to fix it: console.log("333" == 333);
+console.log("333" === 333);
+
+// Wrong result: Expected 555 not 855. Change only the operator to fix it: console.log(600 + 510 / 2);
+console.log((600 + 510) / 2);
+
+// Wrong result: Expected a Number 4 received 22: console.log("2" + 2);
+console.log(2 + 2);
+
+// Create a console.log that returns true when both division results are equal to 5
+// console.log(25 / 5); // Result: 5
+// console.log(50 / 10); // Result: 5
+
+console.log(25 / 5 === 5 && 50 / 10 === 5);
